bc93c379340c5620bbba8587de8728f2d67be78a
Euclides Romero <euclidesantonioromeroibarra@gmail.com>
2024-10-09 22:59:43 -0500

    Añadiendo spinner

--- src/layouts/RutaProtegida.tsx
+++ src/layouts/RutaProtegida.tsx
@@ -1,11 +1,12 @@
 import useAuth from "../hooks/useAuth"
 import { Navigate, Outlet } from "react-router-dom";
+import Loading from "../components/Loading";
 
 
 const RutaProtegida = () => {
   const {auth, loading} = useAuth();
   const { uuid_propietario } = auth;
-  if (loading) return <div>Cargando...</div>; 
+  if (loading) return <div><Loading type= 'spin' color='blue' /> </div>; 
   
   return (
     <>
